04b0ab31bb7c00456d36776f81e52e24a36583a3
Adwaith R Nair <136996663+Adwaith-R-Nair@users.noreply.github.com>
2025-07-24 12:08:17 +0000

    next

---
+++ PythonBasics1/age.py
@@ -0,0 +1,3 @@
+birth_year = int(input('What year were you born? '))
+age = 2025 - birth_year
+print(f'Your age is : {age}')
---
+++ PythonBasics1/boolean.py
@@ -0,0 +1,11 @@
+is_cool = True
+is_cool = False
+print(bool(-0))
+print(bool(0))
+print(bool(1))
+print(bool(0.5))
+print(bool('0'))
+print(bool('True'))
+print(bool('False'))
+print(bool(False))
+print(bool('any random thing'))
