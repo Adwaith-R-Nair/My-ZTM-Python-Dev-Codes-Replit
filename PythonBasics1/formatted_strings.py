9e02c491932e8e629ccc49d9ebde760816b17950
Adwaith R Nair <136996663+Adwaith-R-Nair@users.noreply.github.com>
2025-07-24 10:52:42 +0000

    string changes

--- PythonBasics1/formatted_strings.py
+++ PythonBasics1/formatted_strings.py
@@ -2,3 +2,11 @@ name = "Adwaith"
 age = 22
 
 print(f'Hi {name}. You are {age} years old.')
+
+print("Hello {}, your balance is {}.".format("Cindy", 50))
+
+print("Hello {0}, your balance is {1}.".format("Cindy", 50))
+
+print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+
+print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
